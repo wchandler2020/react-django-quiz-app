8f61152ea704e3ba780c57a14b3decc1f92391a7
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-04-02 09:59:18 -0500

    aaded djangorestframework and added user authentication

--- src/backendQuizApi/settings.py
+++ src/backendQuizApi/settings.py
@@ -26,8 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-    'quizApi'
+    'quizApi',
+
 ]
 
 MIDDLEWARE = [
